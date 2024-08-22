8f8860e504a24dcb46bd177aa15e85d2131b6bb3
naga175 <72788377+naga175@users.noreply.github.com>
2024-08-21 22:44:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-
 app = Flask(__name__)
-
-# Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-db = SQLAlchemy(app)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def helloworld():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run()
